323d405add1a444e4f25098e84e7e5c4e788b3b5
Doyle Jones <doylej1616@gmail.com>
2023-06-03 02:28:14 -0400

    added capstone to projects

--- src/Projects.js
+++ src/Projects.js
@@ -4,8 +4,13 @@ import Navbar from './Navbar';
 const Projects = () => {
   const projects = [
     {
+      title: 'Capstone Project',
+      description: 'My capstone project is a web application that...',
+      deploymentLink: 'https://your-capstone-app.com', // Replace with your deployment link
+    },
+    {
       title: 'Project 1',
-      description: 'A robust backend application for a new mobile telecom company, responsible for efficiently managing customer data and phone bills. Implemented features enabling the addition, editing, and removal of customers from the company database, capturing their first and last name, phone number, city, state, and gender. Seamlessly tracked phone bills for each customer, recording essential details such as ID number, billing amount, minutes used, texts sent, data consumed, and outgoing calls made. Verified API functionality through rigorous testing using Postman.',
+      description: 'A robust backend application for a new mobile telecom company...',
       githubLink: 'https://github.com/Doylej16/backendProject',
       screenshots: [
         '/be1.png',
@@ -17,7 +22,7 @@ const Projects = () => {
     },
     {
       title: 'Project 2',
-      description: 'The CPR Instructor Application is a web platform built with React, Redux, React Router, and Tailwind CSS. It allows CPR instructors to manage courses and offer online training to clients. Key features include user authentication, course creation and management, client tracking, and secure payment processing through Stripe integration.',
+      description: 'The CPR Instructor Application is a web platform built with React, Redux...',
       githubLink: 'https://github.com/Doylej16/CPR-Instructor-Application',
       screenshots: [
         '/screenshot1.png',
@@ -38,12 +43,21 @@ const Projects = () => {
             <div key={index} className="project">
               <h3>{project.title}</h3>
               <p>{project.description}</p>
+              {project.deploymentLink && (
+                <a href={project.deploymentLink} target="_blank" rel="noopener noreferrer">View Deployment</a>
+              )}
+              {project.githubLink && (
+                <a href={project.githubLink} target="_blank" rel="noopener noreferrer">GitHub</a>
+              )}
               <div className="screenshots">
-                {project.screenshots.map((screenshot, idx) => (
-                  <img key={idx} src={process.env.PUBLIC_URL + screenshot} alt={`Project ${index + 1} Screenshot ${idx + 1}`} />
+                {project.screenshots && project.screenshots.map((screenshot, idx) => (
+                  <img
+                    key={idx}
+                    src={process.env.PUBLIC_URL + screenshot}
+                    alt={`Project ${index + 1} Screenshot ${idx + 1}`}
+                  />
                 ))}
               </div>
-              <a href={project.githubLink} target="_blank" rel="noopener noreferrer">GitHub</a>
             </div>
           ))}
         </div>
